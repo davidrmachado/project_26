b1e4b0f5ee8afabe4289a46ddd6b2239b54432f2
David-Neto <davidrmachado@gmail.com>
2023-02-22 21:34:14 -0300

    Entrega do requisito 6

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -26,18 +26,11 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    """Filters a list of jobs by job_type"""
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    jobs_list = []
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    for job in jobs:
+        if job["job_type"] == job_type:
+            jobs_list.append(job)
+    return jobs_list
