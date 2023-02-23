3e83f948c1a855a891e919c6da9fedb4e3af815b
David-Neto <davidrmachado@gmail.com>
2023-02-22 23:05:25 -0300

    Entrega do requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,8 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    """Ensures the correct counting outcome of count_ocurrrences"""
+
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
+    assert count_ocurrences("data/jobs.csv", "javascript") == 122
